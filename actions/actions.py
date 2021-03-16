f74bb496086c6cf2e0b4fe2fb3a560ed9db34713
melindaloubser1 <melinda.loubser@gmail.com>
2021-03-16 11:57:53 +0100

    new flow

--- actions/actions.py
+++ actions/actions.py
@@ -14,6 +14,7 @@ from rasa_sdk.events import (
     ConversationPaused,
     EventType,
     FollowupAction,
+    UserUttered,
 )
 
 from actions import config
@@ -205,6 +206,25 @@ class ActionSetFaqSlot(Action):
             topic = None
 
         return [SlotSet("faq", topic)]
+
+
+class ActionTriggerResponseSelector(Action):
+    """Returns the chitchat utterance dependent on the intent"""
+
+    def name(self) -> Text:
+        return "action_trigger_response_selector"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[EventType]:
+        retrieval_intent = tracker.get_slot("retrieval_intent")
+        if retrieval_intent:
+            dispatcher.utter_message(template=f"utter_{retrieval_intent}")
+
+        return [SlotSet("retrieval_intent", None)]
 
 
 class ActionPause(Action):
